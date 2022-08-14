0058934b24c5e98f4d7181c0d9344d8ee8bc78a1
Edward <edward.nguyen9@gmail.com>
2022-08-15 01:13:36 +0700

    raid init

---
+++ bot/assets/raid.py
@@ -0,0 +1,57 @@
+gods = {
+    'lyx': ['Ouroboros has spawned.', 'The bravest warrior will get a legendary crate!', 793873023245615104], 
+    'jesus': ['Atheistus the Tormentor has returned.', 'The highest bidder among the survivors will get a legendary crate!', 613817172191346726], 
+    'chamburr': ['The giant hamburger has spawned.', 'The highest bidder among the survivors will get a legendary crate!', 606388494922809345], 
+    'monox': ['Mathis Mensing is guarding the top secret cheese factory in Indonesia.', 'One lucky survivor will get a legendary crate!', 793876041323511818], 
+    'eden': ['The guardian to the Garden of Eden has awoken.', 'One lucky survivor will get a legendary crate!', 694110880442482700], 
+    'kirby': ['Dark Mind is attacking Dream Land.', 'Each survivor gets $10,000, and one lucky warrior will also get a legendary crate!', 793886562533769226], 
+    'kvothe': ['The cthae has gathered {number} scrael. They drop money upon being killed.', 'The warrior with the most kills will get a legendary crate!', 606386904157388800],
+    'none': ['A raid has been started.', 'Survive and defeat the boss within 45 minutes for a chance for a legendary crate!', 506133354874404874]
+}
+
+hp = 'This boss has {hp} HP.'
+cash = 'The total payout is ${cash}.'
+link = 'Head to <#{id}> to join the raid.'
+
+wuxi_gif = 'https://i.imgur.com/4tmo6I3.gif'
+half_a_zombie = 'https://i.imgur.com/apqa5bj.jpg'
+full_zombie = 'https://i.imgur.com/EZHrSvx.jpg'
+big_zombie = 'https://i.imgur.com/bxX4de7.jpg'
+
+impostor_bosses = [
+    {
+        "name": "The Absorber",
+        "title": "The Absorber",
+        "gif": "https://i.imgur.com/QW1eu3N.png",
+        "heal": 0.5,
+        "dmg": 1,
+        "def": 1,
+        "hp_mod": 1.1
+    },
+    {
+        "name": "Thanos",
+        "title": "Thanos the Mad Titan",
+        "gif": "https://i.imgur.com/TyybbwU.png",
+        "dmg": 1.5,
+        "def": 1,
+        "hp_mod": 0.85
+    },
+    {
+        "name": "Bomb Voyage",
+        "title": "Bomb Voyage",
+        "gif": "https://i.imgur.com/9iGX8M3.png",
+        "mines": 0.3,
+        "dmg": 1,
+        "def": 1,
+        "hp_mod": 1
+    },
+    {
+        "name": "Dr. Poison",
+        "title": "Dr. Poison",
+        "gif": "https://i.imgur.com/4ukRT9U.png",
+        "psn": 0.1,
+        "dmg": 0.5,
+        "def": 1,
+        "hp_mod": 1
+    }
+]
---
+++ bot/cogs/raid.py
@@ -0,0 +1,21 @@
+import discord
+from discord import app_commands
+from discord.ext import commands, tasks
+
+from bot.assets import raid
+
+class Raid(commands.GroupCog, group_name='raid'):
+    def __init__(self, bot):
+        self.bot = bot
+        self.is_first_ready = True
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if self.is_first_ready:
+            await self.bot.loading()
+            print(self.__class__.__name__, 'is ready')
+            self.is_first_ready = False
+
+
+async def setup(bot):
+    await bot.add_cog(Raid(bot))
